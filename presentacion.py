7bec236bf4d247d95046bed5c70cbd2424cac8e7
Elizabeth Restrepo <elizaretrepo5@gmail.com>
2024-03-23 20:12:16 -0500

    versión2

---
+++ presentacion.py
@@ -0,0 +1,35 @@
+# Definir variables
+
+nombre = "Elizabeth"
+edad = 32
+carrera = "Desarrollo"
+dato1 = input("Ingrese un número ")
+
+presentacion= f"Mi nombre es {nombre} tengo {edad} años, estudio {carrera} y presentare algunas funciones"
+print(presentacion)
+
+#if edad == 32 :
+ #   print("Es verdadero")
+#else:
+#    print("Es falso")
+
+#try:
+ #   dato1 = int(dato1)
+  #  if dato1 > 0:
+   #     print("El número es positivo")
+    #else:
+     #   print("El número es negativo")
+#except ValueError:
+#    print("El número introducido no es válido")
+    
+# operaciones matematicas
+suma = 8 + 9 
+resta = 265 - 25
+multiplicacion = 85 * 64
+division = 8945 / 25
+
+print("suma:", suma)
+print("resta:", resta)
+print("multiplicacion:", multiplicacion)
+print("division:", division)
+
